d380c604087553c36abcb55cf454f7bc05ffc28d
shipperizer <alexcabb@gmail.com>
2023-03-21 20:49:09 +0000

    feat!: move to use go-chi

--- core/api_test.go
+++ core/api_test.go
@@ -3,21 +3,19 @@ package core
 import (
 	"net/http"
 
-	"github.com/gorilla/mux"
-
-	"github.com/shipperizer/miniature-monkey/webiface"
-
 	"testing"
 
+	chi "github.com/go-chi/chi/v5"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
 
 //go:generate mockgen -package core -destination ./mock_webiface.go -source=../webiface/interfaces.go APIInterface
-//go:generate mockgen -package core -destination ./mock_config.go -source=../config/interfaces.go APIConfigInterface,CORSConfigInterface
-//go:generate mockgen -package core -destination ./mock_monitor.go -source=../monitoring/interfaces.go MonitorInterface
+//go:generate mockgen -package core -destination ./mock_config.go -source=../config/interfaces.go CORSConfigInterface
+//go:generate mockgen -package core -destination ./mock_monitor.go -source=../monitoring/core/interfaces.go MonitorInterface
+//go:generate mockgen -package core -destination ./mock_core.go -source=./interfaces.go APIConfigInterface
 
-func TestAPIImplementsAPIInterface(t *testing.T) {
+func TestAPIRouterReturnChiMux(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	mockConfig := NewMockAPIConfigInterface(ctrl)
@@ -25,28 +23,13 @@ func TestAPIImplementsAPIInterface(t *testing.T) {
 	mockConfig.EXPECT().GetServiceName().Times(1).Return("test")
 	mockConfig.EXPECT().GetMonitor().Times(1).Return(nil)
 	mockConfig.EXPECT().GetLogger().Times(1).Return(nil)
+	mockConfig.EXPECT().GetCORSConfig().Times(1).Return(nil)
 
 	newApi := NewAPI(mockConfig)
 
-	assertion := assert.New(t)
+	assert := assert.New(t)
 
-	assertion.Implements((*webiface.APIInterface)(nil), newApi, "object should have implemented APIInterface")
-}
-
-func TestAPIRouterReturnMuxRouter(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-	mockConfig := NewMockAPIConfigInterface(ctrl)
-
-	mockConfig.EXPECT().GetServiceName().Times(1).Return("test")
-	mockConfig.EXPECT().GetMonitor().Times(1).Return(nil)
-	mockConfig.EXPECT().GetLogger().Times(1).Return(nil)
-
-	newApi := NewAPI(mockConfig)
-
-	assertion := assert.New(t)
-
-	assertion.IsType(&mux.Router{}, newApi.Router(), "object should have been of type *mux.Router")
+	assert.IsType(new(chi.Mux), newApi.Router(), "object should have been of type *chi.Mux")
 }
 
 func TestAPIHandlerImplementHttpHandler(t *testing.T) {
@@ -63,9 +46,9 @@ func TestAPIHandlerImplementHttpHandler(t *testing.T) {
 
 	newApi := NewAPI(mockConfig)
 
-	assertion := assert.New(t)
+	assert := assert.New(t)
 
-	assertion.Implements((*http.Handler)(nil), newApi.Handler(), "object should have implemented http.Handler")
+	assert.Implements((*http.Handler)(nil), newApi.Handler(), "object should have implemented http.Handler")
 }
 
 func TestAPIAddStatusAndMonitoring(t *testing.T) {
@@ -74,15 +57,28 @@ func TestAPIAddStatusAndMonitoring(t *testing.T) {
 	mockConfig := NewMockAPIConfigInterface(ctrl)
 	mockMonitor := NewMockMonitorInterface(ctrl)
 
+	mockConfig.EXPECT().GetCORSConfig().Times(1).Return(nil)
 	mockConfig.EXPECT().GetServiceName().Times(1).Return("test")
 	mockConfig.EXPECT().GetMonitor().Times(1).Return(mockMonitor)
 	mockConfig.EXPECT().GetLogger().Times(1).Return(nil)
 	mockMonitor.EXPECT().GetService().Times(1).Return("test")
+	mockMonitor.EXPECT().AddMetrics(gomock.Any()).Times(1).Return(nil)
 
 	newAPI := NewAPI(mockConfig)
 
-	assertion := assert.New(t)
+	assert := assert.New(t)
 
-	assertion.NotNil(newAPI.Router().Get("v1.status"), "object should have a route named v1.status")
-	assertion.NotNil(newAPI.Router().Get("v1.metrics"), "object should have a route named v1.metrics")
+	statusRoute := false
+	metricsRoute := false
+
+	for _, route := range newAPI.Router().Routes() {
+		if route.Pattern == "/api/v1/status" {
+			statusRoute = true
+		} else if route.Pattern == "/api/v1/metrics" {
+			metricsRoute = true
+		}
+	}
+
+	assert.True(statusRoute, "object should have a route named v1.status")
+	assert.True(metricsRoute, "object should have a route named v1.metrics")
 }
